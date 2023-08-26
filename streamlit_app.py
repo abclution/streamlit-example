6fb510f764e5a084cddc3371d0807337875c42e2
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-26 18:45:48 +0300

    fasd

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ max_daily_transactions_BTC = (seconds_per_day * slider_BTC_TPS)
 
 #### BCH TPS SLIDER  ##########################################################
 format_slider_TPS = "%d"
-label_BCH_TPS = '<font size="+5">BCH Network, Maximum Transactions Per Second (Default: 224)</font>' 
+label_BCH_TPS = ':green[BCH Network], Maximum Transactions Per Second *(Default: 224)*' 
 slider_BCH_TPS = st.sidebar.slider(label_BCH_TPS,
                                    1, 10000, 224, 1,
                                    format_slider_TPS)
