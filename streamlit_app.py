67915043d12c158b87f4691fa856ea901076e46e
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-26 17:24:32 +0300

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,23 +123,43 @@ col1.write('Direct method col 1')
 col2.write('Direct method col 2')
 col3.write('Direct method col 3')
 
-with col1:
-    st.header("Setting")
-    st.write('Kilowatt Hours (KwH), Yearly:')
-    st.write('Terawatt Hours (TwH), Yearly:')
+# with col1:
+#     st.header("Setting")
+#     st.write('Kilowatt Hours (KwH), Yearly:')
+#     st.write('Terawatt Hours (TwH), Yearly:')
 
 
+# with col2:
+#     st.header("BTC")
+#     st.write('<font size="+8">', str(round(energyUsageYearlyKwH_BTC)),'</font>', unsafe_allow_html=True)
+#     st.write('<h3>', str(round(slider_energyUsageYearlyTwH_BTC)),'</h3>', unsafe_allow_html=True)
+#     ''' BTC Info here'''
+
+
+# with col3:
+#     st.header("BCH")
+#     st.write('<h3>', str(round(energyUsageYearlyKwH_BCH)),'</h3>', unsafe_allow_html=True)
+#     st.write('<h3>', str(round(energyUsageYearlyTwH_BCH)),'</h3>', unsafe_allow_html=True)
+#     '''BCH Info Here'''
+
+
+
+col1.header("Setting")
+col1.write('Kilowatt Hours (KwH), Yearly:')
+col1.write('Terawatt Hours (TwH), Yearly:')
+
+
+col2.header("BTC")
+col2.write('<font size="+8">', str(round(energyUsageYearlyKwH_BTC)),'</font>', unsafe_allow_html=True)
+col2.write('<h3>', str(round(slider_energyUsageYearlyTwH_BTC)),'</h3>', unsafe_allow_html=True)
 with col2:
-    st.header("BTC")
-    st.write('<font size="+8">', str(round(energyUsageYearlyKwH_BTC)),'</font>', unsafe_allow_html=True)
-    st.write('<h3>', str(round(slider_energyUsageYearlyTwH_BTC)),'</h3>', unsafe_allow_html=True)
     ''' BTC Info here'''
 
 
+col2.header("BCH")
+col2.write('<h3>', str(round(energyUsageYearlyKwH_BCH)),'</h3>', unsafe_allow_html=True)
+col2.write('<h3>', str(round(energyUsageYearlyTwH_BCH)),'</h3>', unsafe_allow_html=True)
 with col3:
-    st.header("BCH")
-    st.write('<h3>', str(round(energyUsageYearlyKwH_BCH)),'</h3>', unsafe_allow_html=True)
-    st.write('<h3>', str(round(energyUsageYearlyTwH_BCH)),'</h3>', unsafe_allow_html=True)
     '''BCH Info Here'''
 
 
