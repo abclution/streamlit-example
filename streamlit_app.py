3e1ee9c3a271a5f8443a20f8ad3b92f81e80b9af
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-26 13:33:43 +0300

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,8 +197,10 @@ df = pd.DataFrame(data)
 st.line_chart(
     data=df,
     x='Electricity Cost (per KwH)',
-    y=None,
-    ("#F2A900", "#0AC18E"))
+    y=['Yearly BTC Security Budget','Yearly BCH Security Budget']
+    )   
+
+    # ("#F2A900", "#0AC18E"))
 
 # # ! This is a simple, linear chart.
 
