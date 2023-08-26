9ade249f56bb5cf05e93a4ac9e78edce0ea9764c
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-26 13:29:47 +0300

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,7 +198,7 @@ st.line_chart(
     data=df,
     x='Electricity Cost (per KwH)',
     y=None,
-    color=("#F2A900", "#0AC18E"))
+    ("#F2A900", "#0AC18E"))
 
 # # ! This is a simple, linear chart.
 
