4d5f62563eaa903ce3f7b6b5a289adc5e039b895
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-26 15:19:16 +0300

    F

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,64 +21,21 @@ farts
 """
 colorBTC = "#F2A900"
 colorBCH = "#0AC18E"
-
-# df = pd.DataFrame(
-#     [
-#         {"command": "st.selectbox", "rating": 4, "is_widget": True},
-#         {"command": "st.balloons", "rating": 5, "is_widget": False},
-#         {"command": "st.time_input", "rating": 3, "is_widget": True},
-#     ]
-# )
-
-# st.dataframe(df, use_container_width=True)
-
-# with st.echo(code_location='below'):
-#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-#     Point = namedtuple('Point', 'x y')
-#     data = []
-
-#     points_per_turn = total_points / num_turns
-
-#     for curr_point_num in range(total_points):
-#         curr_turn, i = divmod(curr_point_num, points_per_turn)
-#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#         radius = curr_point_num / total_points
-#         x = radius * math.cos(angle)
-#         y = radius * math.sin(angle)
-#         data.append(Point(x, y))
-
-#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#         .mark_circle(color='#0068c9', opacity=0.5)
-#         .encode(x='x:Q', y='y:Q'))
-
-
-# st.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
-
-#! st.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
-#! Formatter for int/float supports: %d %e %f %g %i Formatter for date/time/datetime uses Moment.js notation: https://momentjs.com/docs/#/displaying/format/
-
 seconds_per_day = 86400
 
-#### FORMATS #################################################################
-format_slider_TPS = "%d"
-format_slider_energyUsageYearlyTwH = "%d"
+# ! st.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+# ! Formatter for int/float supports: %d %e %f %g %i Formatter for date/time/datetime uses Moment.js notation: https://momentjs.com/docs/#/displaying/format/
 
-format_slider_exaHashes = "%d"
+
+#### FORMATS #################################################################
+
 
 format_slider_Price = "%d"
 
 #### FORMATS #################################################################
 
 #### LABELS #################################################################
-label_BTC_TPS = "BTC Network, Maximum Transactions Per Second (Default: 7)"
-label_BCH_TPS = "BCH Network, Maximum Transactions Per Second (Default: 224)" 
 
-label_energyUsageYearlyTwH_BTC = "BTC: Yearly energy usage of entire network in Terrawatt Hours (Default: 145.02), found here: https://ccaf.io/cbnsi/cbeci " 
-
-label_exaHashes_BTC = "BTC: Current Exahashes (7 Day AVG) (Default: 402.2)"
-label_exaHashes_BCH = "BCH: Current Exahashes (7 Day AVG) (Default: 2.67)"
 
 label_priceBTC = "BTC: Current Price "
 label_priceBCH = "BCH: Current Price"
@@ -86,18 +43,49 @@ label_priceBCH = "BCH: Current Price"
 #### LABELS #################################################################
 
 #### SIDEBAR #################################################################
-slider_BTC_TPS = st.sidebar.slider(label_BTC_TPS, 1, 7, 7, 1, format_slider_TPS)
+
+
+
+#### BTC TPS SLIDER  ##########################################################
+format_slider_TPS = "%d"
+label_BTC_TPS = "BTC Network, Maximum Transactions Per Second (Default: 7)"
+slider_BTC_TPS = st.sidebar.slider(label_BTC_TPS,
+                                   1, 7, 7, 1,
+                                   format_slider_TPS)
 
 st.sidebar.write('Current TPS', '<h2>', str(slider_BTC_TPS), '</h2>', unsafe_allow_html=True)
 
+#### BCH TPS SLIDER  ##########################################################
+format_slider_TPS = "%d"
+label_BCH_TPS = "BCH Network, Maximum Transactions Per Second (Default: 224)" 
+slider_BCH_TPS = st.sidebar.slider(label_BCH_TPS,
+                                   1, 10000, 224, 1,
+                                   format_slider_TPS)
 
-slider_BCH_TPS = st.sidebar.slider(label_BCH_TPS, 1, 10000, 224, 1, format_slider_TPS)
+#### BTC HASHRATE SLIDER  #####################################################
+format_slider_exaHashes = "%d"
+label_exaHashes_BTC = "BTC: Current Exahashes (7 Day AVG) (Default: 402.2)"
+slider_exaHashes_BTC = st.sidebar.slider(label_exaHashes_BTC,
+                                         1.0, 1000.0, 402.2, .1,
+                                         format_slider_exaHashes)
 
-slider_exaHashes_BTC = st.sidebar.slider(label_exaHashes_BTC, 1.0, 1000.0, 402.2, .1, format_slider_exaHashes)
-slider_exaHashes_BCH = st.sidebar.slider(label_exaHashes_BCH, 1.0, 1000.0, 2.67, .1, format_slider_exaHashes)
+#### BCH HASHRATE SLIDER  #####################################################
+format_slider_exaHashes = "%d"
+label_exaHashes_BCH = "BCH: Current Exahashes (7 Day AVG) (Default: 2.67)"
+slider_exaHashes_BCH = st.sidebar.slider(label_exaHashes_BCH,
+                                         1.0, 1000.0, 2.67, .1,
+                                         format_slider_exaHashes)
 
-slider_energyUsageYearlyTwH_BTC = st.sidebar.slider(label_energyUsageYearlyTwH_BTC, 1.0, 1000.0, 145.02, .1, format_slider_energyUsageYearlyTwH)
+#### BTC ENERGY USAGE SLIDER  #################################################
+format_slider_energyUsageYearlyTwH = "%d"
+label_energyUsageYearlyTwH_BTC = "BTC: Yearly energy usage of entire network in Terrawatt Hours (Default: 145.02), found here: https://ccaf.io/cbnsi/cbeci " 
+slider_energyUsageYearlyTwH_BTC = st.sidebar.slider(label_energyUsageYearlyTwH_BTC,
+                                                    1.0, 1000.0, 145.02, .1,
+                                                    format_slider_energyUsageYearlyTwH)
+
+#### BCH ENERGY USAGE SLIDER  #################################################
 # ! No slider for BCH as its energy usage is derived from BTC energy usage per exahash.
+
 energyUsageYearlyKwH_BTC = slider_energyUsageYearlyTwH_BTC * 1000000000
 
 exaHashToYearlyKwHRatio = energyUsageYearlyKwH_BTC / slider_exaHashes_BTC
