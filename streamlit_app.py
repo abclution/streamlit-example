c0d8c3424de76d173dcc98fa7c80c15e7ac78981
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-08-28 17:45:26 +0300

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -526,8 +526,9 @@ with col1:
 
 with col2:
     '''**Now it makes makes sense!**'''
-    ''':green[BCH] cannot compete when its "Hashrate" and security level approach :orange[BTC's] "Hashrate"!!!'''
-        
+    ''':green[BCH] cannot possibly compete when its **"Hashrate"** and security level approach :orange[BTC's] "Hashrate"!!!'''
+    '''~or can it~'''
+    ''' Feel free to adjust :green[BCH's] **"Hashrate"** to match orange[BTC's]**"Hashrate"**'''
     st.write( 'In fact :green[BCHs] security level is exactly:', '<font size="+8">', str(round(((slider_exaHashes_BCH/slider_exaHashes_BTC) * 100), 2)),'%', '</font>', 'of :orange[BTCs]', unsafe_allow_html=True )
 
 
